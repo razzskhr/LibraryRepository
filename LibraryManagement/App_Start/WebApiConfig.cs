7c10ebad2f8dc47b93e5736a14510da5b345e221
Sweta Baid <Sweta.Baid.ext@flydubai.com>
2019-02-07 17:04:31 +0530

    small changes

--- LibraryManagement/App_Start/WebApiConfig.cs
+++ LibraryManagement/App_Start/WebApiConfig.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using System.Web.Http.Cors;
 
 namespace LibraryManagement
 {
@@ -20,9 +19,7 @@ namespace LibraryManagement
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
-            var cors = new EnableCorsAttribute("*", "*", "*");
-            config.EnableCors(cors);
+            
         }
     }
 }
